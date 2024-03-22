92a8dfd27ea080e67d74f6dbbb86bc17531eee23
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2024-03-22 13:34:17 -0300

    Added BaseInterface

---
+++ ApiDeCadastro/Interface/BaseInterface.cs
@@ -0,0 +1,14 @@
+﻿using ApiDeCadastro.Models;
+
+namespace ApiDeCadastro.Interface
+{
+    public interface BaseInterface<T> where T : BaseClass
+    {
+        public void Add(T t);
+        public void Update(Guid Id, T t);
+        public Task<List<T>> List();
+        public T Search(Guid Id);
+        public void Deleted(Guid Id);
+
+    }
+}
---
+++ ApiDeCadastro/Migrations/20240322162926_UserMigration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiDeCadastro.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Surname = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "varchar(128)", maxLength: 128, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Username = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DateCreating = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
