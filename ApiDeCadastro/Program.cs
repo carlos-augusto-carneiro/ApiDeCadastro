816a93f080949721fe30e13034a104d0a4a8af03
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2024-03-22 13:23:21 -0300

    Added connection string in program.cs

--- ApiDeCadastro/Program.cs
+++ ApiDeCadastro/Program.cs
@@ -1,6 +1,11 @@
+using ApiDeCadastro.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultString");
+builder.Services.AddDbContext<DbUser>(opt => opt.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
