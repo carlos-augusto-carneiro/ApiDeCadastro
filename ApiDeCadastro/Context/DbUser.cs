5bda717c41007e787e09a820cfea3b47b96799a7
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2024-03-22 13:16:16 -0300

    Added Context and Mapping is entitys

---
+++ ApiDeCadastro/Context/DbUser.cs
@@ -0,0 +1,16 @@
+﻿using ApiDeCadastro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiDeCadastro.Context;
+
+public class DbUser : DbContext
+{
+    public DbUser(DbContextOptions options) : base(options){}
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserMapping());
+    }
+}
---
+++ ApiDeCadastro/Context/ModelsMapping/UserMapping.cs
@@ -0,0 +1,46 @@
+﻿using ApiDeCadastro.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApiDeCadastro.Context.ModelsMapping;
+
+public class UserMapping : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .IsRequired()
+            .HasColumnName("Id")
+            .HasColumnOrder(1);
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(64)
+            .HasColumnName("Name")
+            .HasColumnOrder(2);
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasMaxLength(128)
+            .HasColumnName("Email")
+            .HasColumnOrder(4);
+        builder.Property(x => x.Surname)
+            .IsRequired()
+            .HasMaxLength(64)
+            .HasColumnName("Surname")
+            .HasColumnOrder(3);
+        builder.Property(x => x.Password)
+            .IsRequired()
+            .HasMaxLength(20)
+            .HasColumnName("Password")
+            .HasColumnOrder(5);
+        builder.Property(x => x.Username)
+            .IsRequired()
+            .HasMaxLength(64)
+            .HasColumnName("Name")
+            .HasColumnOrder(6);
+        builder.Property(x => x.DateCreating)
+            .IsRequired()
+            .HasColumnName("DateCreating")
+            .HasColumnOrder(7);
+    }
+}
